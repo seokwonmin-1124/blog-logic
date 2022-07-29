cacf2a35cb242106c2e541618edcaf8c89161d5e
seokwonmin-1124 <seokwonmin@naver.com>
2022-07-30 00:54:41 +0900

    flask sqlite3 crud 완성

--- main.py
+++ main.py
@@ -1,125 +1,70 @@
 from flask import Flask, request, redirect
+import sqlite3
+import database as db
 
 app = Flask(__name__)
 
-
-nextId = 4
-topics = [
-    {'id': 1, 'title': 'html', 'body': 'html is ...'},
-    {'id': 2, 'title': 'css', 'body': 'css is ...'},
-    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
-]
-
-
-def template(contents, content, id=None):
-    contextUI = ''
-    if id != None:
-        contextUI = f'''
-            <li><a href="/update/{id}/">update</a></li>
-            <li><form action="/delete/{id}/" method="POST"><input type="submit" value="delete"></form></li>
-        '''
-    return f'''
-    <!doctype html>
-    <html>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                {contents}
-            </ol>
-            {content}
-            <ul>
-                <li><a href="/create/">create</a></li>
-                {contextUI}
-            </ul>
-        </body>
-    </html>
-    '''
-
-
-def getContents():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
-    return liTags
-
-
 @app.route('/')
 def index():
-    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+    rows = str(db.readRow())
+    return rows
 
+@app.route('/set/')
+def set():
+    db.setDb()
+    db.readRow(all)
+    return "db is ready"
 
 @app.route('/read/<int:id>/')
 def read(id):
-    title = ''
-    body = ''
-    for topic in topics:
-        if id == topic['id']:
-            title = topic['title']
-            body = topic['body']
-            break
-    return template(getContents(), f'<h2>{title}</h2>{body}', id)
-
+    rows = str(db.readRow(id))
+    return f"""
+    <p>{rows}</p>
+    """
 
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
-    if request.method == 'GET': 
+    if request.method == 'GET':
         content = '''
             <form action="/create/" method="POST">
+                <p><input type="number" name="id" placeholder="id"></p>
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p><textarea name="body" placeholder="body"></textarea></p>
                 <p><input type="submit" value="create"></p>
             </form>
         '''
-        return template(getContents(), content)
+        return content
+
     elif request.method == 'POST':
-        global nextId
+        id = int(request.form['id'])
         title = request.form['title']
         body = request.form['body']
-        newTopic = {'id': nextId, 'title': title, 'body': body}
-        topics.append(newTopic)
-        url = '/read/'+str(nextId)+'/'
-        nextId = nextId + 1
-        return redirect(url)
-
+        db.createRow(id, title, body)
+        return redirect('/')
 
 @app.route('/update/<int:id>/', methods=['GET', 'POST'])
 def update(id):
-    if request.method == 'GET': 
-        title = ''
-        body = ''
-        for topic in topics:
-            if id == topic['id']:
-                title = topic['title']
-                body = topic['body']
-                break
+    if request.method == 'GET':
+        row = db.readRow(id)
         content = f'''
             <form action="/update/{id}/" method="POST">
-                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
-                <p><textarea name="body" placeholder="body">{body}</textarea></p>
+                <p><input type="text" name="title" placeholder="title" value="{row[0][1]}"></p>
+                <p><textarea name="body" placeholder="body">{row[0][2]}</textarea></p>
                 <p><input type="submit" value="update"></p>
             </form>
         '''
-        return template(getContents(), content)
+        return content
+
     elif request.method == 'POST':
-        global nextId
         title = request.form['title']
         body = request.form['body']
-        for topic in topics:
-            if id == topic['id']:
-                topic['title'] = title
-                topic['body'] = body
-                break
-        url = '/read/'+str(id)+'/'
+        db.updateRow(id, title, body)
+        url = f'/read/{id}/'
         return redirect(url)
 
-
-@app.route('/delete/<int:id>/', methods=['POST'])
+@app.route('/delete/<int:id>/')
 def delete(id):
-    for topic in topics:
-        if id == topic['id']:
-            topics.remove(topic)
-            break
+    db.delRow(id)
     return redirect('/')
 
-
-app.run(debug=True)
+app.run(debug=True, port=2020)
