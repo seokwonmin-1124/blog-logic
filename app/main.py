44a5c336e394fcb472b1c9e7bde7b802f4269724
seokwonmin-1124 <seokwonmin@naver.com>
2022-08-02 23:28:33 +0900

    markdown 모듈 추가

--- app/main.py
+++ app/main.py
@@ -1,13 +1,52 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 import database as db
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+Markdown(app, extensions=['nl2br', 'fenced_code'])
+
+ID = "lesh"
+PW = "fptnldlqslek"
+
+login = False
+
+@app.route('/printit')
+def printloginTF():
+    return str(login)
+
+@app.route('/login')
+def login():
+    global login
+    if "userId" in session:
+        login = True
+        return render_template("login.html", userName=session["userId"], login=login)
+    else:
+        login = False
+        return render_template("login.html", login=login)
+
+@app.route('/dologin', methods=['POST'])
+def dologin():
+    _id = request.form['loginId']
+    _pw = request.form['loginPw']
+
+    if ID == _id and PW == _pw:
+        session["userId"] = _id
+        return redirect("/login")
+
+@app.route('/dologout')
+def dologout():
+    session.pop("userId")
+    return redirect("/login")
 
 @app.route('/')
 def index():
+    global login
     rows = db.readRow()
     print(rows)
-    return render_template('index.html', rows=rows)
+    if "userId" in session:
+        login = True
+    return render_template('index.html', rows=rows, login=login)
 
 @app.route('/read/<int:id>/')
 def read(id):
@@ -19,47 +58,48 @@ def read(id):
 
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
-    if request.method == 'GET':
-        content = '''
-            <form action="/create/" method="POST">
-                <p><input type="text" name="title" placeholder="title"></p>
-                <p><textarea name="body" placeholder="body"></textarea></p>
-                <p><input type="submit" value="create"></p>
-            </form>
-        '''
-        return content
+    global login
+    if login == True:
+        if request.method == 'GET':
+            return render_template('create.html')
 
-    elif request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        id = db.createRow(title, body)
-        url = f'/read/{id}/'
-        return redirect(url)
+        elif request.method == 'POST':
+            title = request.form['title']
+            body = request.form['body']
+            id = db.createRow(title, body)
+            url = f'/read/{id}/'
+            return redirect(url)
+
+    else:
+        return redirect("/login")
 
 @app.route('/update/<int:id>/', methods=['GET', 'POST'])
 def update(id):
-    if request.method == 'GET':
-        row = db.readRow(id)
-        content = f'''
-            <form action="/update/{id}/" method="POST">
-                <p><input type="text" name="title" placeholder="title" value="{row[0][1]}"></p>
-                <p><textarea name="body" placeholder="body">{row[0][2]}</textarea></p>
-                <p><input type="submit" value="update"></p>
-            </form>
-        '''
-        return content
+    global login
+    if login == True:
+        if request.method == 'GET':
+            row = db.readRow(id)
+            return render_template('update.html', id=id, title=row[0][1], body=row[0][2])
 
-    elif request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        db.updateRow(id, title, body)
-        url = f'/read/{id}/'
-        return redirect(url)
+        elif request.method == 'POST':
+            title = request.form['title']
+            body = request.form['body']
+            db.updateRow(id, title, body)
+            url = f'/read/{id}/'
+            return redirect(url)
+
+    else:
+        return redirect("/login")
 
 @app.route('/delete/<int:id>/')
 def delete(id):
-    db.delRow(id)
-    return redirect('/')
+    global login
+    if login == True:
+        db.delRow(id)
+        return redirect('/')
+    
+    else:
+        return redirect("/login")
 
 @app.errorhandler(IndexError)
 def indexError(error):
